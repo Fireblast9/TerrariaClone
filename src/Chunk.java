5640b4d7aefed9c1e5b3fa17625913496f637d06
Sasha Toscano <sasha.toscano97@gmail.com>
2025-11-11 16:13:02 +0100

    Fixed Chunk.java

--- src/Chunk.java
+++ src/Chunk.java
@@ -14,27 +14,29 @@ public class Chunk {
     Byte[][][] pzqn;
     Boolean[][][] arbprd;
     Boolean[][] wcnct;
-    Boolean[][] drawn, rdrawn, ldrawn;
+    Boolean[][] drawn;
+    Boolean[][] rdrawn;
+    Boolean[][] ldrawn;
 
     public Chunk(int cx, int cy) {
         this.cx = cx;
         this.cy = cy;
 
         Object[] rv = World.generateChunk(cx, cy, TerrariaClone.getRandom());
-        blocks = (Integer[][][])rv[0];
-        blockds = (Byte[][][])rv[1];
-        blockdns = (Byte[][])rv[2];
-        blockbgs = (Byte[][])rv[3];
-        blockts = (Byte[][])rv[4];
-        lights = (Float[][])rv[5];
-        power = (Float[][][])rv[6];
-        lsources = (Boolean[][])rv[7];
-        zqn = (Byte[][])rv[8];
-        pzqn = (Byte[][][])rv[9];
-        arbprd = (Boolean[][][])rv[10];
-        wcnct = (Boolean[][])rv[11];
-        drawn = (Boolean[][])rv[12];
-        rdrawn = (Boolean[][])rv[13];
-        ldrawn = (Boolean[][])rv[14];
+        blocks = (Integer[][][]) rv[0];
+        blockds = (Byte[][][]) rv[1];
+        blockdns = (Byte[][]) rv[2];
+        blockbgs = (Byte[][]) rv[3];
+        blockts = (Byte[][]) rv[4];
+        lights = (Float[][]) rv[5];
+        power = (Float[][][]) rv[6];
+        lsources = (Boolean[][]) rv[7];
+        zqn = (Byte[][]) rv[8];
+        pzqn = (Byte[][][]) rv[9];
+        arbprd = (Boolean[][][]) rv[10];
+        wcnct = (Boolean[][]) rv[11];
+        drawn = (Boolean[][]) rv[12];
+        rdrawn = (Boolean[][]) rv[13];
+        ldrawn = (Boolean[][]) rv[14];
     }
 }
